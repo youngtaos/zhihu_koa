f6af46b51e9cbebaaf7bd577fc0a38065a3d09ef
youtous <1283374626@qq.com>
2023-03-16 14:40:37 +0800

    使用koa-router实现路由

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
+const usersRouter = new Router({ prefix: '/users' })
 
+router.get('/', (ctx) => {
+    ctx.body = '主页'
+})
+
+usersRouter.get('/', (ctx) => {
+    ctx.body = '用户列表'
+})
+
+usersRouter.post('/', (ctx) => {
+    ctx.body = '创建用户'
+})
+
+usersRouter.get('/:id', (ctx) => {
+    ctx.body = ctx.params.id
+})
+app.use(router.routes())
+app.use(usersRouter.routes())
 app.listen(7001)
